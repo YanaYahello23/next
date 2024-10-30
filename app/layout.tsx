cbf9dc302835095cf508c81eb0de7ba4dded6ad8
YanaYahello <yana.yahello@aioneers.com>
2024-10-30 23:09:12 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {inter} from "./ui/fonts";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+         <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
   );
 }
